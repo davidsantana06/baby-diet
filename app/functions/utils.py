2b9aaa1342423d6ddb4268b9f8991af10cc83bae
davidsantana06 <davidsantana06@hotmail.com>
2023-08-27 19:42:28 -0300

    refactor: general code improve for server functionality

--- app/functions/utils.py
+++ app/functions/utils.py
@@ -10,15 +10,16 @@ def flash_message_repository(name: str) -> tuple[str, str]:
     Retorno:
         tuple[str, str]: Uma tupla com a mensagem e categoria, respectivamente.
     '''
-    match(name):
-        case 'invalid_extension':
-            msg = '<strong>Extensão inválida!</strong> Por favor, selecione um arquivo .xlsx válido.'
-            cat = 'danger'
-        case 'invalid_spreadsheet':
-            msg = '<strong>Planilha inválida!</strong> Por favor, verifique o padrão e preenchimento da tabela.'
-            cat = 'danger'
-        case 'process_spreadsheet':
-            msg = '<b>SUCESSO!</b> As etiquetas foram geradas com base na planilha fornecida.'
-            cat = 'success'
+    msg = cat = ''
+
+    if name == 'invalid_extension':
+        msg = '<strong>Extensão inválida!</strong> Por favor, selecione um arquivo .xlsx válido.'
+        cat = 'danger'
+    elif name == 'invalid_spreadsheet':
+        msg = '<strong>Planilha inválida!</strong> Por favor, verifique o padrão e preenchimento da tabela.'
+        cat = 'danger'
+    elif name == 'process_spreadsheet':
+        msg = '<b>SUCESSO!</b> As etiquetas foram geradas com base na planilha fornecida.'
+        cat = 'success'
 
     return (msg, cat)
